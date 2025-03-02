d71d9a671fcb66d6ea4a094bf52de53ddd356646
--add <--add>
2025-03-01 22:38:29 -0500

    finalizado

--- Calculadora1/test_calculadora.py
+++ Calculadora1/test_calculadora.py
@@ -1,12 +1,19 @@
 import unittest
 from unittest.mock import patch
 from io import StringIO
-from main import calculadora  # Importamos la función desde el script original
+from Calculadora1.main import calculadora  # Importamos la función desde el script original
 
+"""
+    Los test nos permite comprobar el buen funcionammiento de nuestro codigo
+    los test unitario comprueba el funcionamiento por unidad como una clase, un modulo o una funcion
+    nos permite la deteccion de errores y pone a prueba nuestro codigo
+"""
 class TestCalculadora(unittest.TestCase):
 
     @patch("builtins.input", side_effect=["1", "5", "3"])  # Simula elegir "sumar" y los números 5 y 3
     @patch("sys.stdout", new_callable=StringIO)  # Captura la salida en la consola
+    
+    #test para la opcion de suma 
     def test_suma(self, mock_stdout, mock_input):
         calculadora()
         resultado = mock_stdout.getvalue().strip()
